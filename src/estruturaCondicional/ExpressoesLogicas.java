0be9a857b7bc8a26865327cdaf1169547ae4ba93
Felipe Oliveira <fmsoliveira@live.com>
2021-06-07 16:36:17 -0300

    Expressões Logicas

---
+++ src/estruturaCondicional/ExpressoesLogicas.java
@@ -0,0 +1,19 @@
+package estruturaCondicional;
+
+public class ExpressoesLogicas {
+
+	public static void main(String[] args) {
+		/*
+		 * expressoes logicas assim como nas expressoes comparativas, quando são avaliadas resultam em um valor
+		 * verdade (true or false)
+		 * 
+		 * Operadores Logicos
+		 * 
+		 * &&  -- E ( as 2 condições tem que dar true)
+		 * || -- OU ( uma das condições tem que dar true)
+		 * ! -- NAO (nega a condição)
+		 */
+
+	}
+
+}
