c1f25bdfe5e26c344b14fa1d83ac68380d6ed4b1
Felipe Oliveira <fmsoliveira@live.com>
2021-06-07 17:31:40 -0300

    Estrutura If/else

---
+++ src/estruturaCondicional/ExtruturaCondicional.java
@@ -0,0 +1,24 @@
+package estruturaCondicional;
+
+public class ExtruturaCondicional {
+
+	public static void main(String[] args) {
+		/*
+		 * É uma estrutura que decide se um determinado bloco de codigo vai rodar dependendo do resultado de
+		 * uma condição.
+		 * 
+		 * 
+		 * estrutura simples
+		 * 
+		 * if (A > B){
+		 * se o resultado da condição for true, vai executar este bloco
+		 * }
+		 *	else{
+		 *se nao executa este bloco.
+		 *}
+		 * 
+		 */
+
+	}
+
+}
