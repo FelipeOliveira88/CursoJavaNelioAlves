88e3640667a2640b70eb23c9d36181e05b4e68d7
Felipe Oliveira <fmsoliveira@live.com>
2021-06-07 16:12:22 -0300

    Expressões comparativas

---
+++ src/estruturaCondicional/expressoesComparativas.java
@@ -0,0 +1,19 @@
+package estruturaCondicional;
+
+public class expressoesComparativas {
+
+	public static void main(String[] args) {
+		/*
+		 * Operadores:
+		 * 
+		 * < -- menor que
+		 * > -- maior que
+		 * <= -- menor ou igual a
+		 * >= -- maior ou igual a 
+		 * == -- igual
+		 * != -- diferente
+		 */
+
+	}
+
+}
